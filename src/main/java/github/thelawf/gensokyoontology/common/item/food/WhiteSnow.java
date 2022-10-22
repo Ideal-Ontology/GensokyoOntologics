99ac5f2bc99fa839ae7b3ac56d28403d85d42a7f
TheLawF <975523839@qq.com>
2022-10-22 23:30:46 +0800

    Miscellaneous Changes

--- src/main/java/github/thelawf/gensokyoontology/common/item/food/WhiteSnow.java
+++ src/main/java/github/thelawf/gensokyoontology/common/item/food/WhiteSnow.java
@@ -1,9 +1,11 @@
 package github.thelawf.gensokyoontology.common.item.food;
 
+import github.thelawf.gensokyoontology.common.named.GensokyoOntologyNBTs;
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.item.Items;
+import net.minecraft.nbt.CompoundNBT;
 import net.minecraft.potion.EffectInstance;
 import net.minecraft.potion.Effects;
 
@@ -16,5 +18,6 @@ public class WhiteSnow extends Item {
 
     public WhiteSnow() {
         super(new Properties().group(ItemGroup.FOOD).containerItem(Items.BOWL).food(food));
+        this.updateItemStackNBT(GensokyoOntologyNBTs.nbtWhiteSnow);
     }
 }
