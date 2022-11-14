cc6d23014d8ff3104fea8a64a47a970b87799128
TheLawF <975523839@qq.com>
2022-11-14 20:43:26 +0800

    搞了很久的客户端渲染器

---
+++ src/main/java/github/thelawf/gensokyoontology/common/events/ClientEventHandler.java
@@ -0,0 +1,26 @@
+package github.thelawf.gensokyoontology.common.events;
+
+import github.thelawf.gensokyoontology.common.entity.DanmakuEntity;
+import github.thelawf.gensokyoontology.common.entity.render.DanmakuRenderer;
+import github.thelawf.gensokyoontology.core.init.EntityRegistry;
+import net.minecraft.client.renderer.ItemRenderer;
+import net.minecraft.client.renderer.entity.EntityRendererManager;
+import net.minecraft.client.renderer.entity.SpriteRenderer;
+import net.minecraft.entity.EntityType;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.client.registry.IRenderFactory;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD,value = Dist.CLIENT)
+public class ClientEventHandler {
+
+    @SubscribeEvent
+    public static void onClientSetUp(FMLClientSetupEvent event) {
+        ItemRenderer itemRenderer = event.getMinecraftSupplier().get().getItemRenderer();
+        RenderingRegistry.registerEntityRenderingHandler(DanmakuEntity.DANMAKU_TYPE,
+                (EntityRendererManager manager) -> new SpriteRenderer<>(manager, itemRenderer));
+    }
+}
