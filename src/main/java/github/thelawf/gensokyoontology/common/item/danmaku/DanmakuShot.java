7ab79b5ca0ba7c1dd4fb7eb7d2a3a5613f7b651a
TheLawF <975523839@qq.com>
2023-04-22 23:21:25 +0800

    准备加入恋恋的掉落物：恋之瞳，以及妖精的掉落物：空白弹幕

---
+++ src/main/java/github/thelawf/gensokyoontology/common/item/danmaku/DanmakuShot.java
@@ -0,0 +1,9 @@
+package github.thelawf.gensokyoontology.common.item.danmaku;
+
+import net.minecraft.item.Item;
+
+public class DanmakuShot extends Item {
+    public DanmakuShot(Properties properties) {
+        super(properties);
+    }
+}
