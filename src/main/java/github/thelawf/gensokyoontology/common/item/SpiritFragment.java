a98be5316bac2a7e3de66ab9d01cd88bae2ff536
TheLawF <975523839@qq.com>
2023-04-27 09:20:27 +0800

    僵尸和流髑会掉落本模组的基础材料——精神碎片

---
+++ src/main/java/github/thelawf/gensokyoontology/common/item/SpiritFragment.java
@@ -0,0 +1,9 @@
+package github.thelawf.gensokyoontology.common.item;
+
+import net.minecraft.item.Item;
+
+public class SpiritFragment extends Item {
+    public SpiritFragment(Properties properties) {
+        super(properties);
+    }
+}
