e9ea493c34e6dba70b99f54c198913a1bd22c067
TheLawF <975523839@qq.com>
2023-09-27 17:55:43 +0800

    添加了解锁配方的成就

--- src/main/java/github/thelawf/gensokyoontology/common/item/danmaku/DanmakuShotItem.java
+++ src/main/java/github/thelawf/gensokyoontology/common/item/danmaku/DanmakuShotItem.java
@@ -2,23 +2,14 @@ package github.thelawf.gensokyoontology.common.item.danmaku;
 
 import github.thelawf.gensokyoontology.common.util.danmaku.DanmakuType;
 import github.thelawf.gensokyoontology.core.init.itemtab.GSKOCombatTab;
+import net.minecraft.item.Item;
 
-public class DanmakuShotItem extends DanmakuItem {
+public class DanmakuShotItem extends Item {
     public final DanmakuType danmaku;
 
     public DanmakuShotItem(DanmakuType danmaku) {
-        super(new Properties().group(GSKOCombatTab.GSKO_COMBAT_TAB));
+        super(new Item.Properties().group(GSKOCombatTab.GSKO_COMBAT_TAB));
         this.danmaku = danmaku;
     }
-
-    // @Override
-    // @NotNull
-    // public ActionResult<ItemStack> onItemRightClick(@NotNull World worldIn, @NotNull PlayerEntity playerIn, @NotNull Hand handIn) {
-    //     SpellData = new SpellData(null, DanmakuType.DANMAKU_SHOT,
-    //             DanmakuColor.NONE, false, false);
-    //     DanmakuUtil.shootDanmaku(worldIn, playerIn, new DanmakuShotEntity(playerIn, worldIn, spellData),
-    //             0.5f, 0.6f);
-    //     return super.onItemRightClick(worldIn, playerIn, handIn);
-    // }
 
 }
