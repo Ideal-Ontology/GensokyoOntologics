e5fa0fbec92bd8f8c32b51df954cf690c517a613
TheLawF <975523839@qq.com>
2023-09-28 15:19:43 +0800

    修改帕秋莉手册

--- src/main/java/github/thelawf/gensokyoontology/data/recipe/SorceryRecipe.java
+++ src/main/java/github/thelawf/gensokyoontology/data/recipe/SorceryRecipe.java
@@ -1,10 +1,9 @@
 package github.thelawf.gensokyoontology.data.recipe;
 
+import com.google.common.collect.Lists;
 import com.google.gson.JsonArray;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import github.thelawf.gensokyoontology.core.RecipeRegistry;
-import net.minecraft.block.Blocks;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.crafting.*;
@@ -14,21 +13,29 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
 import net.minecraftforge.registries.ForgeRegistryEntry;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.Map;
-
-public class SorceryRecipe implements ISorceryRecipe{
+public class SorceryRecipe implements ISorceryRecipe {
 
     private final ResourceLocation id;
     private final ItemStack output;
+    private final ItemStack up;
+    private final ItemStack left;
+    private final ItemStack right;
+    private final ItemStack down;
 
-    private final NonNullList<Ingredient> ingredients;
+    private final NonNullList<Ingredient> inputs;
 
-    public SorceryRecipe(ResourceLocation id, ItemStack output, NonNullList<Ingredient> ingredients) {
+    public SorceryRecipe(ResourceLocation id, ItemStack output, ItemStack up, ItemStack left, ItemStack right, ItemStack down) {
         this.id = id;
+        this.up = up;
+        this.left = left;
+        this.right = right;
+        this.down = down;
+
         this.output = output;
-        this.ingredients = ingredients;
+        inputs = NonNullList.withSize(4, Ingredient.fromItems(up.getItem(), left.getItem(), right.getItem(), down.getItem()));
     }
 
     @Override
@@ -51,8 +58,9 @@ public class SorceryRecipe implements ISorceryRecipe{
         return this.id;
     }
 
+    @NotNull
     public NonNullList<Ingredient> getIngredients() {
-        return ingredients;
+        return NonNullList.withSize(4, Ingredient.fromItems(up.getItem(), left.getItem(), right.getItem(), down.getItem()));
     }
 
     @Override
@@ -73,13 +81,13 @@ public class SorceryRecipe implements ISorceryRecipe{
         public SorceryRecipe read(ResourceLocation recipeId, JsonObject json) {
             ItemStack output = ShapedRecipe.deserializeItem(JSONUtils.getJsonObject(json, "output"));
             JsonArray ingredients = JSONUtils.getJsonArray(json, "ingredients");
-            NonNullList<Ingredient> inputs = NonNullList.withSize(4, Ingredient.EMPTY);
 
-            for (int i = 0; i < inputs.size(); i++) {
-                inputs.set(i, Ingredient.deserialize(ingredients.get(i)));
-            }
+            ItemStack upStack = JSONUtils.getItem(json, "up").getDefaultInstance();
+            ItemStack leftStack = JSONUtils.getItem(json, "left").getDefaultInstance();
+            ItemStack rightStack = JSONUtils.getItem(json, "right").getDefaultInstance();
+            ItemStack downStack = JSONUtils.getItem(json, "down").getDefaultInstance();
 
-            return new SorceryRecipe(recipeId, output, inputs);
+            return new SorceryRecipe(recipeId, output, upStack, leftStack, rightStack, downStack);
         }
 
         @Nullable
@@ -90,13 +98,17 @@ public class SorceryRecipe implements ISorceryRecipe{
             inputs.replaceAll(ignored -> Ingredient.read(buffer));
 
             ItemStack output = buffer.readItemStack();
-            return new SorceryRecipe(recipeId, output, inputs);
+            return new SorceryRecipe(recipeId, output,
+                    Ingredient.read(buffer).getMatchingStacks()[0],
+                    Ingredient.read(buffer).getMatchingStacks()[1],
+                    Ingredient.read(buffer).getMatchingStacks()[2],
+                    Ingredient.read(buffer).getMatchingStacks()[3]);
         }
 
         @Override
         public void write(PacketBuffer buffer, SorceryRecipe recipe) {
-            buffer.writeVarInt(recipe.ingredients.size());
-            for (Ingredient ingredient : recipe.ingredients) {
+            buffer.writeVarInt(recipe.inputs.size());
+            for (Ingredient ingredient : recipe.inputs) {
                 ingredient.write(buffer);
             }
             buffer.writeItemStack(recipe.getRecipeOutput(), false);
