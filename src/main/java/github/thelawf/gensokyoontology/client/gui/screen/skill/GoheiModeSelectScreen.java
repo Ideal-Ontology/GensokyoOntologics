9926f1eef09ca2a86d109a19e31a02df77439eb6
TheLawF <975523839@qq.com>
2024-05-21 00:41:02 +0800

    现在可以用alt调出御币的切换界面了

--- src/main/java/github/thelawf/gensokyoontology/client/gui/screen/skill/GoheiModeSelectScreen.java
+++ src/main/java/github/thelawf/gensokyoontology/client/gui/screen/skill/GoheiModeSelectScreen.java
@@ -1,6 +1,7 @@
 package github.thelawf.gensokyoontology.client.gui.screen.skill;
 
 import com.mojang.blaze3d.matrix.MatrixStack;
+import com.mojang.blaze3d.systems.RenderSystem;
 import github.thelawf.gensokyoontology.GensokyoOntology;
 import github.thelawf.gensokyoontology.common.item.touhou.HakureiGohei;
 import github.thelawf.gensokyoontology.common.network.GSKONetworking;
@@ -9,6 +10,7 @@ import github.thelawf.gensokyoontology.common.util.EnumUtil;
 import net.minecraft.client.gui.screen.GamemodeSelectionScreen;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import org.jetbrains.annotations.NotNull;
@@ -18,44 +20,47 @@ public class GoheiModeSelectScreen extends MultiSelectScreen{
     private HakureiGohei.Mode mode;
     private int guiLeft = 0;
     private int guiTop = 0;
-    public static final ITextComponent DANMAKU = GensokyoOntology.withTranslation("gui.", "gohei.mode.danmaku");
-    public static final ITextComponent DREAM_SEAL = GensokyoOntology.withTranslation("gui.", "gohei.mode.dream_seal");
+    private int xSize;
+    private int ySize;
+    public static final TranslationTextComponent DANMAKU = GensokyoOntology.withTranslation("gui.", ".gohei.mode.danmaku");
+    public static final TranslationTextComponent DREAM_SEAL = GensokyoOntology.withTranslation("gui.", ".gohei.mode.dream_seal");
     public static final ResourceLocation TEXTURE = GensokyoOntology.withRL("textures/gui/gohei_selection_screen.png");
     public GoheiModeSelectScreen(ITextComponent titleIn, HakureiGohei.Mode mode) {
         super(titleIn);
         this.mode = mode;
-        this.width = 64;
-        this.height = 32;
+        this.xSize = 64;
+        this.ySize = 64;
     }
 
     @Override
     protected void init() {
         super.init();
-        this.guiLeft = (this.width - 32) / 2;
-        this.guiTop = (this.height - 32) / 2;
-
+        this.guiLeft = (this.width - this.xSize) / 2;
+        this.guiTop = (this.height - this.ySize) / 2;
     }
 
     @Override
     public void render(@NotNull MatrixStack matrixStack, int mouseX, int mouseY, float partialTicks) {
-        super.render(matrixStack, mouseX, mouseY, partialTicks);
         if (this.minecraft == null) return;
         matrixStack.push();
+        RenderSystem.enableBlend();
         this.minecraft.getTextureManager().bindTexture(TEXTURE);
 
-        this.blit(matrixStack, this.guiLeft, this.guiTop, 0, 0, 32, 32);
-        this.blit(matrixStack, this.guiLeft + 10, this.guiTop, 32, 0, 32, 32);
-
-        this.font.drawTextWithShadow(matrixStack, DANMAKU, this.guiLeft, this.guiTop + 42, 16777215);
-        this.font.drawTextWithShadow(matrixStack, DREAM_SEAL, this.guiLeft + 10, this.guiTop + 42, 16777215);
-
         if (mode == HakureiGohei.Mode.DANMAKU) {
-            this.blit(matrixStack, this.guiLeft, this.guiTop, 64, 0, 32, 32);
+            this.blit(matrixStack, this.guiLeft, this.guiTop, 0, 32, 32, 32);
+            this.blit(matrixStack, this.guiLeft + 42, this.guiTop, 32, 0, 32, 32);
         }
         else {
-            this.blit(matrixStack, this.guiLeft + 10, this.guiTop, 64, 0, 32, 32);
+            this.blit(matrixStack, this.guiLeft, this.guiTop, 0, 0, 32, 32);
+            this.blit(matrixStack, this.guiLeft + 42, this.guiTop, 32, 32, 32, 32);
         }
+
         matrixStack.pop();
+
+        this.font.drawTextWithShadow(matrixStack, DANMAKU, this.guiLeft, this.guiTop + 42, 16777215);
+        this.font.drawTextWithShadow(matrixStack, DREAM_SEAL, this.guiLeft + 42, this.guiTop + 42, 16777215);
+
+
     }
 
     @Override
