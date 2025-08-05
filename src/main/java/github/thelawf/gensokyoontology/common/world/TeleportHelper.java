2e21b066807b2abcebdbdda46ad413a00a11f772
TheLawF <975523839@qq.com>
2025-08-05 17:44:44 +0800

    修复了传送的问题

--- src/main/java/github/thelawf/gensokyoontology/common/world/TeleportHelper.java
+++ src/main/java/github/thelawf/gensokyoontology/common/world/TeleportHelper.java
@@ -3,10 +3,12 @@ package github.thelawf.gensokyoontology.common.world;
 import com.mojang.datafixers.util.Pair;
 import github.thelawf.gensokyoontology.common.tileentity.GapTileEntity;
 import github.thelawf.gensokyoontology.common.util.GSKOUtil;
+import github.thelawf.gensokyoontology.common.world.dimension.biome.GSKOBiomes;
 import github.thelawf.gensokyoontology.core.init.BlockRegistry;
 import net.minecraft.advancements.criterion.AbstractCriterionTrigger;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.command.impl.LocateBiomeCommand;
 import net.minecraft.command.impl.TeleportCommand;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.ServerPlayerEntity;
@@ -21,12 +23,13 @@ import net.minecraftforge.common.util.ITeleporter;
 import net.minecraftforge.fluids.IFluidBlock;
 import org.apache.logging.log4j.LogManager;
 
+import javax.annotation.Nullable;
 import java.util.function.Function;
 
 public class TeleportHelper {
     public static void teleport(ServerPlayerEntity player, ServerWorld destination, BlockPos pos) {
-        GSKOUtil.showChatMsg(player, "Can TP: " + canTeleport(destination, pos), 1);
-        if (canTeleport(destination, pos)) {
+        GSKOUtil.showChatMsg(player, "Can TP: " + canTeleport(player, destination, pos), 1);
+        if (canTeleport(player, destination, pos)) {
             player.changeDimension(destination, new ITeleporter() {
                 @Override
                 public Entity placeEntity(Entity entity, ServerWorld currentWorld, ServerWorld destWorld,
@@ -69,28 +72,33 @@ public class TeleportHelper {
     /**
      * 获取目的地世界对应位置的方块状态，如果玩家位置是空气且玩家脚下的方块是固态方块则进行传送
      */
-    public static boolean canTeleport(ServerWorld destination, BlockPos pos) {
+    public static boolean canTeleport(ServerPlayerEntity player, ServerWorld destination, BlockPos pos) {
         if (destination == null)
             return false;
 
-        BlockPos lowerPos = new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ());
-        BlockPos upperPos = new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ());
-        BlockState eyeBlock = destination.getBlockState(upperPos);
-        BlockState playerBlock = destination.getBlockState(pos);
-        BlockState standBlock = destination.getBlockState(lowerPos);
+        BlockPos standPos = new BlockPos(pos.getX(), pos.getY() - 1, pos.getZ());
+        BlockPos eyePos = new BlockPos(pos.getX(), pos.getY() + 1, pos.getZ());
+        BlockState eyeBlock = destination.getBlockState(eyePos);
+        BlockState legBlock = destination.getBlockState(pos);
+        BlockState standBlock = destination.getBlockState(standPos);
 
-        if (eyeBlock.equals(Blocks.AIR.getDefaultState()) && playerBlock.equals(Blocks.AIR.getDefaultState())) {
+        GSKOUtil.showChatMsg(player, "Eye is " + eyeBlock.getBlock().getTranslatedName().getString(), 1);
+        GSKOUtil.showChatMsg(player, "Leg is " + legBlock.getBlock().getTranslatedName().getString(), 1);
+        GSKOUtil.showChatMsg(player, "Stands " + standBlock.getBlock().getTranslatedName().getString(), 1);
+
+        if (eyeBlock.equals(Blocks.AIR.getDefaultState()) && legBlock.equals(Blocks.AIR.getDefaultState())) {
             if (standBlock.getBlock().equals(Blocks.AIR)) {
-                destination.setBlockState(lowerPos, BlockRegistry.SAKURA_PLANKS.get().getDefaultState());
-                LogManager.getLogger().info("Position Valid");
+                // GSKOUtil.showChatMsg(player, "Pos Valid", 1);
+                // destination.setBlockState(standPos, BlockRegistry.SAKURA_PLANKS.get().getDefaultState());
+                setBlocks(destination, pos);
                 return true;
             } else if (standBlock.getBlock() instanceof IFluidBlock) {
-                destination.setBlockState(lowerPos, BlockRegistry.SAKURA_PLANKS.get().getDefaultState());
-                LogManager.getLogger().info("Stand Position Has Fluids");
+                // destination.setBlockState(standPos, BlockRegistry.SAKURA_PLANKS.get().getDefaultState());
+                // GSKOUtil.showChatMsg(player, "Stand Pos is Fluid", 1);
+                setBlocks(destination, pos);
                 return true;
-            } else {
-                return isValidPos(destination, pos);
             }
+            return true;
         } else {
             if (standBlock.getBlock().equals(Blocks.AIR)) {
                 return clearAndSetBlocks(destination, pos);
@@ -104,11 +112,11 @@ public class TeleportHelper {
         final BlockState sakuraPlanks = BlockRegistry.SAKURA_PLANKS.get().getDefaultState();
         destination.setBlockState(pos, air);
 
-        for (int x = 0; x < 2; x++) {
-            for (int z = 0; z < 2; z++) {
+        for (int x = 0; x < 3; x++) {
+            for (int z = 0; z < 3; z++) {
                 destination.setBlockState(pos.toMutable().move(x, pos.getY(), z), sakuraPlanks);
                 destination.setBlockState(pos.toMutable().move(-x, pos.getY(), -z), sakuraPlanks);
-                for (int y = 0; y < 2; y++) {
+                for (int y = 0; y < 3; y++) {
                     destination.setBlockState(pos.toMutable().move(x, y, z), air);
                     destination.setBlockState(pos.toMutable().move(-x, y, -z), air);
                 }
@@ -121,12 +129,24 @@ public class TeleportHelper {
         final BlockState air = Blocks.AIR.getDefaultState();
         destination.setBlockState(pos, air);
 
-        for (int i = 0; i < 2; i++) {
-            for (int j = 0; j < 2; j++) {
-                for (int k = 0; k < 2; k++) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
                     destination.setBlockState(pos.toMutable().move(i, j, k), air);
                     destination.setBlockState(pos.toMutable().move(-i, j, -k), air);
                 }
+            }
+        }
+        return true;
+    }
+
+    private static boolean setBlocks(ServerWorld destination, BlockPos pos){
+        final BlockState sakuraPlanks = BlockRegistry.SAKURA_PLANKS.get().getDefaultState();
+        for (int x = 0; x < 3; x++) {
+            for (int z = 0; z < 3; z++) {
+                destination.setBlockState(pos.toMutable().move(x, pos.getY(), z), sakuraPlanks);
+                destination.setBlockState(pos.toMutable().move(-x, pos.getY(), -z), sakuraPlanks);
+
             }
         }
         return true;
@@ -139,13 +159,13 @@ public class TeleportHelper {
 
     private static Pair<Boolean, BlockPos> findValidPos(ServerWorld destination, BlockPos pos) {
         for (int i = 0; i < 255; i++) {
-            BlockPos playerPos = new BlockPos(pos.getX(), i, pos.getZ());
+            BlockPos legPos = new BlockPos(pos.getX(), i, pos.getZ());
             BlockPos standPos = new BlockPos(pos.down().getX(), i, pos.down().getZ());
             BlockPos eyePos = new BlockPos(pos.up().getX(), i, pos.up().getZ());
             if (!destination.getBlockState(standPos).getBlock().equals(Blocks.AIR) &&
-                    destination.getBlockState(playerPos).getBlock().equals(Blocks.AIR) &&
+                    destination.getBlockState(legPos).getBlock().equals(Blocks.AIR) &&
                     destination.getBlockState(eyePos).getBlock().equals(Blocks.AIR)) {
-                return Pair.of(true, playerPos);
+                return Pair.of(true, legPos);
             }
         }
         return Pair.of(false, pos);
@@ -154,4 +174,5 @@ public class TeleportHelper {
     private static boolean isInSameDimension(RegistryKey<World> departureWorld, RegistryKey<World> destination) {
         return departureWorld == destination;
     }
+
 }
