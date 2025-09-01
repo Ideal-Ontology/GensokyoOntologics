2200ba2c858dd252e88de35795ecb3c4ee8146c6
Fictology <law662607@163.com>
2025-09-02 03:12:02 +0800

    需要修改：尺寸

--- src/main/java/github/thelawf/gensokyoontology/common/entity/misc/LunarFall.java
+++ src/main/java/github/thelawf/gensokyoontology/common/entity/misc/LunarFall.java
@@ -4,13 +4,18 @@ import github.thelawf.gensokyoontology.common.capability.GSKOCapabilities;
 import github.thelawf.gensokyoontology.common.entity.AffiliatedEntity;
 import github.thelawf.gensokyoontology.common.util.GSKODamageSource;
 import github.thelawf.gensokyoontology.core.init.EntityRegistry;
+import net.minecraft.client.particle.DragonBreathParticle;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.GlassBottleItem;
+import net.minecraft.network.IPacket;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.world.Explosion;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.network.NetworkHooks;
+import org.jetbrains.annotations.NotNull;
 
 public class LunarFall extends AffiliatedEntity {
     public static final int PREPARATION = 45;
@@ -36,26 +41,35 @@ public class LunarFall extends AffiliatedEntity {
             this.remove();
             return;
         }
+
         if (this.ticksExisted == PREPARATION) {
             this.world.createExplosion(this, this.getPosX(), this.getPosY(), this.getPosZ(), 3, Explosion.Mode.NONE);
             this.world.getEntitiesWithinAABB(LivingEntity.class, this.getBoundingBox()).stream().filter(
                     this::isEntityInRange).forEach(entity -> {
                 if (this.getOwner() == null) return;
                 this.getOwner().getCapability(GSKOCapabilities.POWER).ifPresent(power ->
-                        entity.attackEntityFrom(DamageSource.MAGIC, power.getCount() * 8));
+                        entity.attackEntityFrom(DamageSource.MAGIC, power.getCount() * 6));
             });
 
             if (this.ticksExisted > PREPARATION) {
                 double amount = (this.ticksExisted - PREPARATION);
                 this.setBoundingBox(this.getBoundingBox().expand(amount, 0.0D, amount));
+                // this.getType().getSize().scale()
+                this.recalculateSize();
 
                 this.world.getEntitiesWithinAABB(LivingEntity.class, this.getBoundingBox())
                         .stream().filter(this::isEntityInRange).forEach(entity -> {
                             if (this.getOwner() == null) return;
                             GSKODamageSource.causeImpact(this.getOwner(), entity, 0.3F);
                         });
+
             }
         }
+    }
+
+    @Override
+    public @NotNull IPacket<?> createSpawnPacket() {
+        return  NetworkHooks.getEntitySpawningPacket(this);
     }
 
     public boolean isEntityInRange(LivingEntity living) {
